1dd71c0917b2be9a43be483548c34642a6d61cbf
jhabarsingh <jhabarsinghbhati23@gmail.com>
2021-05-26 12:09:25 +0530

    Added app for hosting

---
+++ dist/precache-manifest.209cec1babeb1eeb8a71613de75b15ca.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb70167d72c0b9aae4579a896305b13e",
+    "url": "/DNS_Configuration.csv"
+  },
+  {
+    "revision": "c5c5819cef4efa3162ec",
+    "url": "/css/about.7cc7de02.css"
+  },
+  {
+    "revision": "acc429ee9d8612616028",
+    "url": "/css/chunk-vendors.bc0aa964.css"
+  },
+  {
+    "revision": "eb513551a480899bccab61d5a2cf4cf2",
+    "url": "/img/Home_page.eb513551.jpg"
+  },
+  {
+    "revision": "e1dfe5b83e798c78dd0ba9dfbfc5c3da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5c5819cef4efa3162ec",
+    "url": "/js/about.3d28b984.js"
+  },
+  {
+    "revision": "13016a0ccc6eb7efc263",
+    "url": "/js/app.6598185d.js"
+  },
+  {
+    "revision": "acc429ee9d8612616028",
+    "url": "/js/chunk-vendors.da8906f1.js"
+  },
+  {
+    "revision": "63e5804e9bbc72d148f1b7aab49e1a43",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
